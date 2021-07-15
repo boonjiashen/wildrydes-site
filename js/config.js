744808c5bc5553194e52c32ec4634aa4d5e69d8f
Jia-Shen <jiashen@gmail.com>
2021-07-16 07:23:09 +0900

    Add cognito settings
    Following https://aws.amazon.com/getting-started/hands-on/build-serverless-web-app-lambda-apigateway-s3-dynamodb-cognito/module-2/
    
    __Testing done__
    
    1. `python -m http.server`
    1. Visit `localhost:8000` on browser. Hard refresh to clear the browser's JS cache
    1. Tested signing up and logging in.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_5Yzgs4vcn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '70u61n5el036ic1rtkr2sf5ie4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
